e41a56aacd41612d98c456e6fa5d9520fae36598
Osama.Ahmadi <osama.alahmadi90@gmail.com>
2025-12-26 17:17:39 +0300

    a

--- src/main/java/com/example/homeway/Service/MyUserDetailsService.java
+++ src/main/java/com/example/homeway/Service/MyUserDetailsService.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+//m
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
